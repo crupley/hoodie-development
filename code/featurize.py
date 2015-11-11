132aaa34cea1fbdf6716cfc83982abc17c2f937d
crupley <crupley@gmail.com>
2015-11-10 16:39:19 -0800

    implement featurizer class

--- code/featurize.py
+++ code/featurize.py
@@ -187,6 +187,123 @@ def feature_permutations(levels):
 	# pickle.load(open('data/bin_overlap.pkl', 'rb'))
 
 
+class featurizer():
+
+	def __init__(self):
+		self.features = pd.DataFrame()
+
+		self.latmin = 37.70784
+		self.latmax = 37.8195
+		self.lonmin = -122.5185
+		self.lonmax = -122.35454
+
+		self.latbins = np.linspace(self.latmin, self.latmax, 101)
+		self.lonbins = np.linspace(self.lonmin, self.lonmax, 101)
+
+		df = get_db('usc_shapefile')
+		df = df[df.lat > self.latmin]
+		df = df[df.lat < self.latmax]
+		df = df[df.lon > self.lonmin]
+		df = df[df.lon < self.lonmax]
+		self.shapefile = df
+
+
+
+	def set_limits(self, latmin, latmax, lonmin, lonmax):
+		self.latmin = latmin
+		self.latmax = latmax
+		self.lonmin = lonmin
+		self.lonmax = lonmax
+
+
+	def set_resolution(self, npoints):
+		self.latbins = np.linspace(latmin, latmax, npoints)
+		self.lonbins = np.linspace(lonmin, lonmax, npoints)
+
+	def add_features(self, flist, how='usc', verbose=False):
+
+		if how == 'usc':
+			self.latbins = self.shapefile.lat
+			self.lonbins = self.shapefile.lon
+
+		for f in flist:
+			if verbose: print 'loading ', db; sys.stdout.flush()
+
+			# load database table
+			df1 = get_db(f)
+
+			# merge in lat/lon for census data from shapefile
+			if f in ['usc_age_gender', 'usc_household', 'usc_pop']:
+				df1 = df1.merge(self.shapefile, left_on = 'id2',
+								right_on = 'geoid')
+
+			# handling each table type
+			if f == 'assessment':
+				df1 = df1[['lat_cut', 'lon_cut', 'taxable_value']]
+				df1 = df1.groupby(['lat_cut', 'lon_cut']).mean()
+				df1 = df1.reset_index().dropna()
+
+			# elif f == 'business':
+			# 	df1 = df1[['lat_cut', 'lon_cut', 'category']]
+			# 	df1['count'] = 1
+			# 	df1 = df1.groupby(['lat_cut', 'lon_cut', 'category']).count()
+			# 	df1 = df1.reset_index().dropna()
+			# 	df2 = df1.pivot(columns = 'category', values = 'count').fillna(0)
+			# 	df1 = df1.merge(df2, left_index=True, right_index=True)
+			# 	df1.drop('category', axis = 1, inplace = True)
+			# 	df1 = df1.groupby(['lat_cut', 'lon_cut']).sum().reset_index()
+			# 	df1 = df1[['lat_cut', 'lon_cut', 'grocery',
+			# 			   'restaurant', 'retail']]
+
+			# elif f == 'sfpd':
+			# 	df1['ncrimes'] = 1
+			# 	df1 = df1.groupby(['lat_cut', 'lon_cut']).count()
+			# 	df1 = df1.dropna().reset_index()
+			# 	df1 = df1[['lat_cut', 'lon_cut', 'ncrimes']]
+
+			elif f == 'usc_age_gender':
+				df1['sgnf'] = (2 * df1.f / df1.total).fillna(0) - 1
+				df1 = df1[['lat', 'lon', 'sgnf']]
+
+			# elif f == 'usc_household':
+
+			# 	# calc average household size
+			# 	total_p = 0
+			# 	p_range = range(1,8)
+			# 	for i in p_range:
+			# 		col = 'p' + str(i)
+			# 		total_p += df1[col] * i
+			# 	av_p = total_p / df1.total
+			# 	df1['avg_hh_size'] = av_p
+			# 	df1.fillna(0, inplace = True)
+
+			# 	df1 = df1[['lat', 'lon', 'avg_hh_size']]
+
+			# elif f == 'usc_pop':
+			# 	df1 = df1.groupby(['lat_cut', 'lon_cut']).sum()
+			# 	df1 = df1.dropna().reset_index()
+			# 	df1 = df1[['lat_cut', 'lon_cut', 'total']]
+			# 	df1.columns = ['lat', 'lon', 'pop']
+
+			# elif f == 'walkscore':
+			# 	df1 = df1[['lat', 'lon', 'walkscore']]
+
+			finterp = bin_interpolate(df1.lon, df1.lat, df1.iloc[:,2],
+									  self.lonbins, self.latbins)
+			finterp = pd.Series(finterp)
+
+			# append results to final data frame
+			if self.features.shape == (0, 0):
+				self.features = df2 = pd.concat((self.latbins, self.lonbins,
+												 finterp), axis = 1)
+				self.features.columns = df1.columns
+			else:
+				self.features = pd.concat((self.features, finterp), axis = 1)
+				self.features.rename(columns = {0: df1.columns[-1]})
+
+
+
+
 
 if __name__ == '__main__':
 	res = feature_permutations()
