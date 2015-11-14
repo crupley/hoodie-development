218be5c53aefce02687c19ca930115db96c5924f
crupley <crupley@gmail.com>
2015-11-14 09:06:55 -0800

    modify GN output reporting

--- code/communities.py
+++ code/communities.py
@@ -1,6 +1,7 @@
 import networkx as nx
 from collections import Counter
 import sys
+from time import time
 
 
 def girvan_newman_step(graph):
@@ -12,10 +13,17 @@ def girvan_newman_step(graph):
     Afterwards, the graph will have one more connected component.
     '''
     size = nx.number_connected_components(graph)
+    edges = graph.number_of_edges()
+    biggroup = len(max(nx.connected_components(graph), key=len))
     cur = 0
     while cur <= size:
-        most_connected = Counter(nx.edge_betweenness_centrality(graph)).most_common(1)[0][0]
-        print most_connected; sys.stdout.flush()
+        most_connected = Counter(nx.edge_betweenness_centrality(graph, weight='sim')).most_common(1)[0][0]
+        node1 = most_connected[0]
+        node2 = most_connected[1]
+        outitems = (node1, node2, size, edges, biggroup, time())
+        outs = '%d, %d, %d, %d, %d, %f\n' % outitems
+        print outs; sys.stdout.flush()
+        #print most_connected; sys.stdout.flush()
         graph.remove_edge(*most_connected)
         cur = nx.number_connected_components(graph)
 
@@ -50,7 +58,7 @@ def find_communities_modularity(G, max_iter=None):
     best_comps = nx.connected_components(G1)
     i = 0
     while G1.number_of_edges() > 0:
-        print G1.number_of_edges(), len(max(nx.connected_components(G1), key=len)); sys.stdout.flush()
+        #print G1.number_of_edges(), len(max(nx.connected_components(G1), key=len)); sys.stdout.flush()
         subgraphs = nx.connected_component_subgraphs(G1)
         modularity = get_modularity(subgraphs, degrees, num_edges)
         if modularity > best_modularity:
