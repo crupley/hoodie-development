a0498b7eb6ddab72860c5cf9d2fb893a564771ff
crupley <crupley@gmail.com>
2015-11-14 15:02:24 -0800

    graph info and make graph

--- code/graph.py
+++ code/graph.py
@@ -19,4 +19,15 @@ def make_edges(latlondf):
 	    edges.index.name = 'edge'
 	return edges.astype('int')
 
+def graph_info(graph):
+	print nx.info(graph)
+	print 'Number of clusters:', nx.number_connected_components(graph)
+	print 'Largest cluster:', len(max(nx.connected_components(graph),
+									  key=len))
+
+def make_graph(edgesdf, name=None):
+	g = nx.from_pandas_dataframe(edgesdf, source='node1',
+								 target='node2', edge_attr='fdist')
+	if name:
+		g.add_node('namenode', name=name)
 
