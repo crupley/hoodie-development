1f94e75fca6fd1f6d9407b00703db683069ad46b
crupley <crupley@gmail.com>
2015-11-14 09:33:26 -0800

    test aws.graph_reduce

--- code/aws.py
+++ code/aws.py
@@ -1,28 +1,33 @@
 import networkx as nx
 from collections import Counter
-import sys
 from time import time
 
 
-def girvan_newman_step(graph):
+def graph_reduce(graph, max_size=1):
     '''
-    INPUT: Graph G
+    INPUT: Graph G, int max_size
     OUTPUT: None
 
-    Run one step of the Girvan-Newman community detection algorithm.
-    Afterwards, the graph will have one more connected component.
+    Sequentially remove most highly connected nodes until largest cluster
+    is max_size and output sequence to file.
     '''
-    size = nx.number_connected_components(graph)
-    edges = graph.number_of_edges()
-    biggroup = len(max(nx.connected_components(graph), key=len))
-    cur = 0
-    while cur <= size:
-        most_connected = Counter(nx.edge_betweenness_centrality(graph, weight='sim')).most_common(1)[0][0]
+    g = graph.copy()
+    fn = str(g.node['namenode']['name']) + '.csv'
+    with open('results/' + fn, 'wb') as f:
+        f.write('node1,node2,size,edges,biggroup,timestamp\n')
+
+    biggroup = len(max(nx.connected_components(g), key=len))
+
+    while biggroup >= max_size:
+        most_connected = Counter(nx.edge_betweenness_centrality(g, weight='sim')).most_common(1)[0][0]
         node1 = most_connected[0]
         node2 = most_connected[1]
+        size = nx.number_connected_components(g)
+        edges = g.number_of_edges()
+        biggroup = len(max(nx.connected_components(g), key=len))
         outitems = (node1, node2, size, edges, biggroup, time())
-        outs = '%d, %d, %d, %d, %d, %f\n' % outitems
-        print outs; sys.stdout.flush()
-        #print most_connected; sys.stdout.flush()
-        graph.remove_edge(*most_connected)
-        cur = nx.number_connected_components(graph)
+        outs = '%d,%d,%d,%d,%d,%f\n' % outitems
+        # print outs
+        with open('results/' + fn, 'a') as f:
+            f.write(outs)
+        g.remove_edge(*most_connected)
