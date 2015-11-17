8931849741c81db3c4e05ea1ab679efa7c5540ac
crupley <crupley@gmail.com>
2015-11-17 11:40:22 -0800

    calculate wcss vs k

--- code/assessment.py
+++ code/assessment.py
@@ -3,6 +3,22 @@ import numpy as np
 import pandas as pd
 import networkx as nx
 
+from code.featurize import sim
+
+def bigsize(row):
+    g.remove_edge(row.node1, row.node2)
+    return len(max(nx.connected_components(g), key=len))
+
+
+def cutcon(row):
+    g.remove_edge(row.node1, row.node2)
+    return nx.number_connected_components(g)
+
+
+def cutrow(row, graph):
+    graph.remove_edge(row.node1, row.node2)
+    return
+
 def assign_clusters(df, graph):
     cc = list(nx.connected_components(graph))
     
@@ -11,3 +27,17 @@ def assign_clusters(df, graph):
         for i, cluster in enumerate(cc):
             if row in cluster: cnum.ix[row] = i
     return cnum
+
+
+def row_errorsq(row, cluster_means):
+    rowf = row.drop(['cnum'])
+    return (sim(rowf, cluster_means.ix[int(row.cnum)]))**2
+
+
+def wcss(df):
+    dfcp = df.copy()
+    features = df.drop(['lat', 'lon'], axis = 1)
+    cluster_means = features.groupby('cnum').mean()
+    dfcp['errors'] = features.apply(lambda x: row_errorsq(x, cluster_means), axis = 1)
+
+    return dfcp.groupby('cnum').sum()['errors']
