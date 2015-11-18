424efb5fbf6215be29e28d7fe1ac0aef2f1f4b4f
crupley <crupley@gmail.com>
2015-11-17 16:05:23 -0800

    correct preduce spelling

--- code/preduce.py
+++ code/preduce.py
@@ -5,7 +5,7 @@ import cPickle as pickle
 from code.graphreduce import graph_reduce_gt
 
 def f(g):
-	fn = 'results/' + g.gp.Name
+	fn = 'results/' + g.gp.Name + '.csv'
 	c = graph_reduce_gt(g, fn)
 	return c
 
@@ -14,5 +14,5 @@ if __name__ == '__main__':
 
 	gs = pickle.load(open('g4.pkl', 'rb'))
 
-	p = Pool(process=4)
+	p = Pool(processes=2)
 	outputs = p.map(f, gs)
